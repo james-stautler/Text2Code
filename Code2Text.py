c8f6b24dd34381f46c1fab152bc087bb4c3d0a7c
James Stautler <jstautler10@gmail.com>
2024-08-03 13:09:23 -0500

    python script

---
+++ Code2Text.py
@@ -0,0 +1,32 @@
+
+print("Enter/Paste your code. Ctrl-Z and enter on the final line to save it.")
+contents = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    contents.append(line)
+
+outputCode = contents[0]
+for line in contents[1:]:
+    outputCode += "\\n"
+    
+    if not line: 
+        continue
+
+    spaceCount = 0
+    index = 0
+    while index < len(line) and line[index] == " ":
+        spaceCount += 1
+        index += 1
+    line = line[index:]
+
+    for i in range(spaceCount // 4):
+        outputCode += "\\t"
+    
+    outputCode += line
+
+print(outputCode)
+
+input("Press any key to exit")
